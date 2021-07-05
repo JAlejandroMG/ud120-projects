1cbebe0ee6f6082d1039df89ab55dae68f63e7ce
JAlejandroMG <jalejandromg@tutanota.com>
2021-07-04 22:41:10 -0500

    Add Support Vector Machines solution

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,37 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
+#clf = SVC(kernel="linear")
+#clf = SVC(kernel="rbf")
+clf = SVC(kernel="rbf", C=10000)
+
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+predicted = clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
+
+#Counting emails from Chris (Sara = 0)
+chris = 0
+for i in predicted:
+    if (i == 1):
+        chris += i
+print(chris)
+
+#print(clf.predict(features_test[10:11]))
+#print(clf.predict(features_test[26:27]))
+#print(clf.predict(features_test[50:51]))
+
+accuracy = accuracy_score(predicted, labels_test)
+print(accuracy)
 #########################################################
 
 
